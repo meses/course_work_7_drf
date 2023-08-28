1fec1f77976961df6f7f0c112fd171be3081b30f
Artem <akirillov@itps-russia.ru>
2023-08-28 11:30:13 +0500

    Начальная настройка проекта

---
+++ users/models.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+class CustomUserManager(UserManager):
+
+    def _create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='email')
+    phone = models.CharField(max_length=35, verbose_name='Телефон', **NULLABLE)
+    city = models.CharField(max_length=50, verbose_name='Город', **NULLABLE)
+    avatar = models.ImageField(upload_to='avatar/', verbose_name='Аватар', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
