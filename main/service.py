38cc2f70bacf3e2972bdbbb1bb3220c246881cb9
Artem <akirillov@itps-russia.ru>
2023-09-06 18:14:13 +0500

    Донастройка работы с Telegram

--- main/service.py
+++ main/service.py
@@ -4,13 +4,23 @@ from config import settings
 
 telegram_api_token = settings.TELEGRAM_API_TOKEN
 chat_id = settings.CHAT_ID
-message = "Напоминание о привычке"
+#message = "Напоминание о привычке"
+#
+#url = f"https://api.telegram.org/bot{telegram_api_token}/sendMessage"
+#params = {
+#    "chat_id": chat_id,
+#    "text": message
+#}
+#
+#response = requests.get(url, params=params)
+#print(response.json())
 
-url = f"https://api.telegram.org/bot{telegram_api_token}/sendMessage"
-params = {
-    "chat_id": chat_id,
-    "text": message
-}
-
-response = requests.get(url, params=params)
-print(response.json())
+def send_telegram_message(habit):
+    message = f"Ваша привычка: {habit.user.telegram_username}:\nМесто: {habit.place}\nДействие: {habit.action}\nВремя: {habit.time}"
+    url = f"https://api.telegram.org/bot{telegram_api_token}/sendMessage"
+    params = {
+        "chat_id": chat_id,
+        "text": message
+    }
+    response = requests.get(url, params=params)
+    return response.json()
