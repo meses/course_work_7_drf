ecc604504846ad5771665802a9a364dca25e9fd7
Artem <akirillov@itps-russia.ru>
2023-09-07 21:46:34 +0500

    Описание проекта

--- main/service.py
+++ main/service.py
@@ -4,16 +4,16 @@ from config import settings
 
 telegram_api_token = settings.TELEGRAM_API_TOKEN
 chat_id = settings.CHAT_ID
-#message = "Напоминание о привычке"
+# message = "Напоминание о привычке"
 #
-#url = f"https://api.telegram.org/bot{telegram_api_token}/sendMessage"
-#params = {
+# url = f"https://api.telegram.org/bot{telegram_api_token}/sendMessage"
+# params = {
 #    "chat_id": chat_id,
 #    "text": message
-#}
+# }
 #
-#response = requests.get(url, params=params)
-#print(response.json())
+# response = requests.get(url, params=params)
+# print(response.json())
 
 
 def send_telegram_message(habit):
